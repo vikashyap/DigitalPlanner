6e5f923e351e0e28f549863cea377366ab7d4598
vikkashyap <kashyapvikas20@gmail.com>
2023-08-08 11:24:49 +0200

    netlify build config

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import { useRouter } from "next/navigation";
 //Note: Only using default export for root page
 export default function Page() {
   const router = useRouter();
-  router.push("/pension-estimator");
+  if (typeof window !== "undefined") {
+    router.push("/pension-estimator");
+  }
 
   return null;
 }
